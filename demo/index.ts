f5615cbad4f78ccd67464db3201e53c2e1858269
Simon Huber <simon.huber@renuo.ch>
2018-09-09 20:45:13 +0200

    Make it ready for library usage

---
+++ demo/index.ts
@@ -0,0 +1,10 @@
+import { Translations } from './generated/i18n-interface'
+import { allTranslations } from './generated/i18n-translations'
+import { I18n } from '../lib/i18n/i18n'
+
+// GENERATE THE FILES:
+// `generate-i18n {path-to-get-json-translation-files} {path-to-generate-interfaces}`
+// `../scripts/generate-i18n i18n generated`
+
+const i18n = new I18n<Translations>('de', allTranslations);
+console.log(i18n.t.another);
